12bc5105a4db619fcb3c88cf25364d59b7ad6fb3
Rafael Gil Pastor <rgilpast@gmail.com>
2015-10-16 22:56:21 +0200

    first version

---
+++ JuegoDeMemoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JuegoDeMemoria
+//
+//  Created by Rafael Gil Pastor on 16/10/15.
+//  Copyright © 2015 Rafael Gil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
